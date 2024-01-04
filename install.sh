cdc2f2121de157517f46fd139c4716dcb8d2a057
Glenn Sorrentino <git@scidsg.org>
2024-01-03 19:55:27 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@ sudo apt install python3 python3-pip git nginx default-mysql-server python3-venv
 cd /var/www/html
 git clone https://github.com/glenn-sorrentino/hosted-line
 cd hosted-line
-git switch settings
+git switch pgp
 cd ..
 mv hosted-line hushline-hosted
 cd hushline-hosted
@@ -28,7 +28,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 # Install Flask, Gunicorn, and other Python libraries
-pip install Flask pymysql python-dotenv gunicorn Flask-SQLAlchemy Flask-Bcrypt pyotp qrcode
+pip install Flask pymysql python-dotenv gunicorn Flask-SQLAlchemy Flask-Bcrypt pyotp qrcode python-gnupg
 
 SECRET_KEY=$(python3 -c 'import os; print(os.urandom(64).hex())')
 
